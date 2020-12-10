81c9b15eecbcdbf9c30901d9accd9e4b50868140
evan <evan@evanw.org>
2020-12-10 17:10:11 -0600

    accept number list file

--- README.md
+++ README.md
@@ -5,4 +5,16 @@ Look up registrants in North America Number Plan Adminstrator
 ## quickstart
 
     git clone http://github.com/evidlo/nanpa_lookup && cd nanpa_lookup
-    ./lookup.sh 907-200-1234
+    ./lookup.sh -n 907-200-1234
+    
+## usage
+
+    # provide a number
+    ./lookup.sh -n 907-200-1234
+    ./lookup.sh -n 9072001234
+
+    # or provide line separated list of numbers
+    ./lookup.sh -f path/to/numbers.txt
+    
+    # get a list of unique registrants
+    ./lookup.sh -f path/to/numbers.txt | sort | uniq
--- lookup.sh
+++ lookup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Evan Widloski - 2020-12-10
+# Look up phone registrants in NANPA database
 
-# usage: ./lookup.sh 123-456-0987
+# usage: ./lookup.sh -n 123-456-0987
+# usage: ./lookup.sh -f numbers.txt
 
 # download database
 if [ ! -f allutlzd.txt ]
@@ -9,7 +12,27 @@ then
     unzip allutlzd.zip
 fi
 
-NUMBER=$(sed 's/-//' <<< $1)
-NPANXX=${NUMBER:0:3}-${NUMBER:3:3}
+# look up NPA-NXX code in database
+lookup() {
+    grep -Fw -f npanxx.txt allutlzd.txt | cut -d $'\t' -f 4
+}
 
-awk -v npanxx=$NPANXX -F $'\t' '$2 == npanxx {print $4}' allutlzd.txt
+# convert number to NPA-NXX code
+parse_number() {
+    NUMBER=$(sed 's/-//' <<< $1)
+    echo ${NUMBER:0:3}-${NUMBER:3:3}
+}
+
+if [[ $1 == "-f" ]]
+then
+    rm -f npanxx.txt
+    while read number
+    do
+        parse_number $number >> npanxx.txt
+        lookup
+    done < "$2"
+else
+    parse_number $2 > npanxx.txt
+    lookup
+fi
+
